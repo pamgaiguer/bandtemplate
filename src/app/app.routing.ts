f713b6f06265edc078dd562411dff360be6368a8
Pam Gaiguer <pamella.gaiguer@gmail.com>
2020-04-02 22:52:40 +1000

    rename band

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -14,7 +14,7 @@ const routes: Routes =[
     { path: 'components',             component: ComponentsComponent },
     { path: 'user-profile',     component: ProfileComponent },
     { path: 'signup',           component: SignupComponent },
-    { path: 'landing',          component: LandingComponent },
+    { path: 'home',          component: LandingComponent },
     { path: 'nucleoicons',      component: NucleoiconsComponent }
 ];
 
