70e8c97a47c6454b06e9a8dfae4360b2f5ae2892
Pam Gaiguer <pamella.gaiguer@gmail.com>
2022-03-08 23:28:24 +1000

    updated to angular 11 - starting migration to 12

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -41,8 +41,9 @@ const routes: Routes =[
     CommonModule,
     BrowserModule,
     RouterModule.forRoot(routes,{
-      useHash: false
-    })
+    useHash: false,
+    relativeLinkResolution: 'legacy'
+})
   ],
   exports: [
   ],
