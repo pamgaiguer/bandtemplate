8e06ce1d0fcf49d85c3ddb00162f0c72150b2612
Pam Gaiguer <pamella@quantiz.com.br>
2024-10-18 09:48:25 -0300

    updating to angular 18

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Inject, Renderer2, ElementRef, ViewChild } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
-import { Subscription } from 'rxjs/Subscription';
-import 'rxjs/add/operator/filter';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
 import { DOCUMENT } from '@angular/common';
 import { LocationStrategy, PlatformLocation, Location } from '@angular/common';
 import { NavbarComponent } from './shared/navbar/navbar.component';
@@ -16,18 +16,22 @@ export class AppComponent implements OnInit {
     private _router: Subscription;
     @ViewChild(NavbarComponent) navbar: NavbarComponent;
 
-    constructor( 
-        private renderer : Renderer2, 
-        private router: Router, 
-        @Inject(DOCUMENT,) 
-        private document: any, 
-        private element : ElementRef, 
+    constructor(
+        private renderer : Renderer2,
+        private router: Router,
+        @Inject(DOCUMENT,)
+        private document: any,
+        private element : ElementRef,
         public location: Location
-        ) {}
+    ) {
+
+    }
 
     ngOnInit() {
         var navbar : HTMLElement = this.element.nativeElement.children[0].children[0];
-        this._router = this.router.events.filter(event => event instanceof NavigationEnd).subscribe((event: NavigationEnd) => {
+        this._router = this.router.events.pipe(
+            filter(event => event instanceof NavigationEnd)
+        ).subscribe((event: NavigationEnd)=>{
             if (window.outerWidth > 991) {
                 window.document.children[0].scrollTop = 0;
             }
@@ -36,6 +40,7 @@ export class AppComponent implements OnInit {
             }
             this.navbar.sidebarClose();
         });
+
         this.renderer.listen('window', 'scroll', (event) => {
             const number = window.scrollY;
 
